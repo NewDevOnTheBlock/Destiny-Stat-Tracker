f584fe751fc1c32724adf58c7a6e14dd73002919
Pierce <piercedeanda@gmail.com>
2023-03-16 11:43:57 -0700

    first commit, basic setup of create-react-app done, installed react-router-dom and sass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import Header from './layout/Header';
+import HomePage from './layout/HomePage';
+import NotFound from './layout/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} exact />
+        <Route element={<NotFound />} />
+      </Routes>
+    </main>
   );
 }
 
