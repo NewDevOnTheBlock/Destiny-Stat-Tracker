ce996269fb1c9a4ecbbba68b01f8ac567e90bd6e
Keithan Van <105301483+NVious7@users.noreply.github.com>
2023-05-24 19:54:33 -0400

    Update HomePage.js

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -1,12 +1,17 @@
 import React from 'react';
+require('dotenv').config();
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 let code=urlParams.get('code');
 console.log(code);
 
-function HomePage() {
-    return <a href='https://www.bungie.net/en/OAuth/Authorize?response_type=code&client_id=43378'>Authorize Me!</a>
+async function HomePage() {
+    if (code) {
+        let response = await fetch(`https://destiny2appbackend.onrender.com/?code=${code}`);
+        console.log(response);
+    }
+    return <a href={`https://www.bungie.net/en/OAuth/Authorize?response_type=code&client_id=43378`}>Authorize Me!</a>
 }
 
 export default HomePage;
