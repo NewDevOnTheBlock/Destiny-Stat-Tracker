d14807a836680de55ae83e598b55221f840a2f5f
Keithan Van <105301483+NVious7@users.noreply.github.com>
2023-05-24 19:15:58 -0400

    Update HomePage.js

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let code=urlParams.get('code');
+console.log(code);
+
 function HomePage() {
-    return <h1>HomePage</h1>
+    return <a href='https://www.bungie.net/en/OAuth/Authorize?response_type=code&client_id=43378'>Authorize Me!</a>
 }
 
 export default HomePage;
